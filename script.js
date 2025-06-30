471f7bc01da1771db2a83677af2382bad9fe70bf
Adam Khan <adammkhan47@gmail.com>
2025-06-30 12:15:31 -0500

    added dark mode

---
+++ script.js
@@ -0,0 +1,8 @@
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
